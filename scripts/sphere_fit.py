ef42a6cb0f1c7b14436b09f9b2647b988b69dd36
ThomasABourget <150868757+ThomasABourget@users.noreply.github.com>
2024-04-03 16:39:45 -0400

    Update sphere_fit.py

--- scripts/sphere_fit.py
+++ scripts/sphere_fit.py
@@ -1,78 +1,68 @@
 #!/usr/bin/env python3
-
-#I dont know why this didnt work I kept getthing the same error over and over. I know I need to change the values of the x,y,z to a similiar format of the hw7 from 340 however 
-#i dont know how to change that to the proper form with the context of this code.
-
-
 import rospy
 from robot_vision_lectures.msg import XYZarray, SphereParams
-import numpy as np
+from turtlesim.msg import Pose
 
+pos_msg = Pose()
+pos_received = False
 
-
-def fit_sphere(x, y, z):
-    # Construct matrices A and B
-    A = np.vstack((x, y, z, np.ones_like(x))).T
-    B = x**2 + y**2 + z**2
-
-    # Solve for P
-    P, _, _, _ = np.linalg.lstsq(A, B, rcond=None)
-
-    # Extract center coordinates
-    x_c, y_c, z_c, _ = 0.5 * P
-
-    # Calculate radius
-    radius = np.sqrt(P[3] + x_c**2 + y_c**2 + z_c**2)
-    return x_c, y_c, z_c, radius
-
+def pose_callback(data):
+    global pos_msg, pos_received
+    pos_msg = data
+    pos_received = True
 
 def xyz_callback(data):
+    global x_c_previous, y_c_previous, z_c_previous, radius_previous
     x = []
     y = []
     z = []
-    
     for point in data.points:
         x.append(point.x)
         y.append(point.y)
         z.append(point.z)
-
-    # Convert to numpy arrays
     x = np.array(x)
     y = np.array(y)
     z = np.array(z)
-
-    # Fit sphere to the data
     x_c, y_c, z_c, radius = fit_sphere(x, y, z)
-
-    # Publish the parameters of the fitted sphere
+    x_c_filtered = low_pass_filter(x_c, x_c_previous)
+    y_c_filtered = low_pass_filter(y_c, y_c_previous)
+    z_c_filtered = low_pass_filter(z_c, z_c_previous)
+    radius_filtered = low_pass_filter(radius, radius_previous)
+    x_c_previous = x_c_filtered
+    y_c_previous = y_c_filtered
+    z_c_previous = z_c_filtered
+    radius_previous = radius_filtered
     sphere_params_msg = SphereParams()
-    sphere_params_msg.xc = x_c
-    sphere_params_msg.yc = y_c
-    sphere_params_msg.zc = z_c
-    sphere_params_msg.radius = radius
-    sphere_pub.publish(sphere_params_msg)  # Publishing should use sphere_pub
-    print(sphere_params_msg)
+    sphere_params_msg.xc = x_c_filtered
+    sphere_params_msg.yc = y_c_filtered
+    sphere_params_msg.zc = z_c_filtered
+    sphere_params_msg.radius = radius_filtered
+    sphere_pub.publish(sphere_params_msg)
+
 if __name__ == '__main__':
     rospy.init_node('sphere_fit', anonymous=True)
     xyz_sub = rospy.Subscriber('/xyz_cropped_ball', XYZarray, xyz_callback)
     sphere_pub = rospy.Publisher('/sphere_params', SphereParams, queue_size=10)
-    rate = rospy.Rate(10)
+
+    pos_sub = rospy.Subscriber('/turtle1/noisy_pose', Pose, pose_callback)
+    pos_pub = rospy.Publisher('/turtle1/filtered_pose', Pose, queue_size=10)
+
+    loop_rate = rospy.Rate(10)
+
+    fil_in = 0.0
+    fil_out = 5.5
+    fil_gain = 0.05
+
+    x_c_previous = 0.0
+    y_c_previous = 0.0
+    z_c_previous = 0.0
+    radius_previous = 0.0
 
     while not rospy.is_shutdown():
-        # Spin to receive messages
-        rospy.spin()
+        if pos_received:
+            fil_in = pos_msg.x
+            fil_out = fil_gain * fil_in + (1 - fil_gain) * fil_out
+            pos_msg.x = fil_out
+            pos_pub.publish(pos_msg)
 
-	
-if __name__ == '__main__':
-	rospy.init_node('sphere_fit', anonymous = True)
-	xyz_sub = rospy.Subscriber('/xyz_cropped_ball', XYZarray, xyz_callback)
-	sphere_pub = rospy.Publisher('/sphere_params',SphereParams, queue_size = 10)
-	rate = rospy.Rate(10)
-
-	while not rospy.is_shutdown():
-		# make sure we process if the camera has started streaming images
-		# pause until the next iteration			
-		rate.sleep()
-	
-
-
+        loop_rate.sleep()
